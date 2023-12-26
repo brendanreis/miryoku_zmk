9cf032046c31363e18b6b77743aa5bc2e4d0dc7c
brendanreis <brendanreis@gmail.com>
2023-12-25 21:38:41 -0500

    fix

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -3,10 +3,11 @@
 
 // Graphite: https://github.com/rdavison/graphite-layout
 #define MIRYOKU_LAYER_BASE \
-&kp B,             &kp L,             &kp D,             &kp W,             &kp Z,             &kp SQT,           &kp F,             &kp O,             &kp U,             &kp J,           \
-&kp N,             &kp R,             &kp T,             &kp S,             &kp G,             &kp Y,             &kp H,             &kp A,             &kp E,             &kp I,           \
-&kp Q,             &kp X,             &kp M,             &kp C,             &kp V,             &kp K,             &kp P,             &kp COMMA,         &kp DOT,           &kp SLASH,       \
-U_NP,              U_NP,              &kp ESC,           &kp SPACE,         &kp TAB,           &kp RET,           &kp BSPC,          &kp DEL,           U_NP,              U_NP
+&kp B,             &kp L,             &kp D,                &kp W,             &kp Z,              &kp SQT,           &kp F,             &kp O              &kp U,             &kp J,                       \
+U_MT(LGUI, N),     U_MT(LALT, R),     U_MT(LCTRL, T),       U_MT(LSHFT, S),    &kp G,              &kp Y,             U_MT(LSHFT, H),    U_MT(LCTRL, A),    U_MT(LALT, E),     U_MT(LGUI, I),               \
+U_LT(U_BUTTON, Q), U_MT(RALT, X),     &kp M,                &kp C,             &kp V,              &kp K,             &kp P,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),       \
+U_NP,              U_NP,              U_LT(U_MEDIA, ESC),   U_LT(U_NAV, SPACE),U_LT(U_MOUSE, TAB), U_LT(U_SYM, RET),  U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),  U_NP,              U_NP
+
 
 // TODO Romak: https://github.com/rafaelromao/romak
 // #define MIRYOKU_LAYER_BASE \
