0f7a6033408988fb58eb144fefb10b9094060c49
brendanreis <brendanreis@gmail.com>
2023-12-25 21:22:06 -0500

    push graphite as base

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,3 +1,16 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
+// Graphite: https://github.com/rdavison/graphite-layout
+#define MIRYOKU_LAYER_BASE \
+&kp B,             &kp L,             &kp D,             &kp W,             &kp Z,             &kp SQT,           &kp F,             &kp O,             &kp U,             &kp J,           \
+&kp N,             &kp R,             &kp T,             &kp S,             &kp G,             &kp Y,             &kp H,             &kp A,             &kp E,             &kp I,           \
+&kp Q,             &kp X,             &kp M,             &kp C,             &kp V,             &kp K,             &kp P,             &kp COMMA,         &kp DOT,           &kp SLASH,       \
+U_NP,              U_NP,              &kp ESC,           &kp SPACE,         &kp TAB,           &kp RET,           &kp BSPC,          &kp DEL,           U_NP,              U_NP
+
+// TODO Romak: https://github.com/rafaelromao/romak
+// #define MIRYOKU_LAYER_BASE \
+// &kp B,             &kp L,             &kp D,             &kp W,             &kp Z,             &kp SQT,           &kp F,             &kp O,             &kp U,             &kp J,           \
+// &kp N,             &kp R,             &kp T,             &kp S,             &kp G,             &kp Y,             &kp H,             &kp A,             &kp E,             &kp I,           \
+// &kp Q,             &kp X,             &kp M,             &kp C,             &kp V,             &kp K,             &kp P,             &kp DOT,           &kp COMMA,         &kp SLASH,       \
+// U_NP,              U_NP,              &kp ESC,           &kp SPACE,         &kp TAB,           &kp RET,           &kp BSPC,          &kp DEL,           U_NP,              U_NP
