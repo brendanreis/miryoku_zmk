2a8852d4068a02248b7b38fccc286b88ae713ac6
brendanreis <brendanreis@gmail.com>
2024-07-20 18:00:08 -0400

    fix compilation

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -65,4 +65,3 @@ K00  K01  K02  K03  K04  &kp NUM_8  XXX  K05  K06  K07  K08  K09 \
 K10  K11  K12  K13  K14  &kp NUM_9  XXX  K15  K16  K17  K18  K19 \
 K20  K21  K22  K23  K24  &kp NUM_0  XXX  K25  K26  K27  K28  K29 \
           XXX  K32  K33  K34        K35  K36  K37  XXX
-#endif
