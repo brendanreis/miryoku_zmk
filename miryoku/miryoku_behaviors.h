a370f37815f14242062bcfa457dacb4530ceabd8
brendanreis <brendanreis@gmail.com>
2025-04-30 01:03:28 -0400

    maybe fix

--- miryoku/miryoku_behaviors.h
+++ miryoku/miryoku_behaviors.h
@@ -6,3 +6,6 @@
 #define U_MT_L(MOD, TAP) &u_mt_l MOD TAP
 #define U_MT_R(MOD, TAP) &u_mt_l MOD TAP
 #define U_LT(LAYER, TAP) &u_lt LAYER TAP
+
+/* Don't use this one */
+#define U_MT(MOD, TAP) &u_mt_l MOD TAP
