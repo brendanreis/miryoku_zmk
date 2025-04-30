e349f0716bdb148cfc3d26331ce52bf4db7bb007
brendanreis <brendanreis@gmail.com>
2025-04-30 01:04:02 -0400

    adjust values per urob config

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -3,8 +3,9 @@
 
 #define MIRYOKU_CLIPBOARD_WIN
 #define MIRYOKU_TAP_QWERTY
-#define U_TAPPING_TERM 100
-#define U_REQUIRE_PRIOR_IDLE 60
+#define U_TAPPING_TERM 280
+#define U_QUICK_TAP 175
+#define U_REQUIRE_PRIOR_IDLE 150
 
 #define LEFT_HAND_KEYS      \
 0  1  2  3  4               \
