41b198a772a74083de2b76b4c5ed3eefd5e1ccfa
brendanreis <brendanreis@gmail.com>
2025-04-29 23:38:01 -0400

    Revert "use hold-preferred for shift"
    This reverts commit 6fcefb9377b9da190126c745f9f14826a792a043.

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -9,7 +9,7 @@
 // My modification of Graphite: https://github.com/rdavison/graphite-layout
 #define MIRYOKU_LAYER_BASE \
 &kp B,             &kp L,             &kp D,             &kp W,             &kp Z,             &kp SQT,             &kp F,             &kp O,             &kp U,             &kp J,           \
-U_MT(LCTRL, N),     U_MT(LALT, R),     U_MT(LGUI, T),    U_ST(LSHFT, S),    &kp G,             &kp Y,             U_ST(LSHFT, H),    U_MT(LGUI, A),    U_MT(LALT, E),     U_MT(LCTRL, I),     \
+U_MT(LCTRL, N),     U_MT(LALT, R),     U_MT(LGUI, T),    U_MT(LSHFT, S),    &kp G,             &kp Y,             U_MT(LSHFT, H),    U_MT(LGUI, A),    U_MT(LALT, E),     U_MT(LCTRL, I),     \
 U_LT(U_BUTTON, Q), U_MT(RALT, X),     &kp M,             &kp C,             &kp V,             &kp K,             &kp P,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
 U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, SPACE),U_LT(U_MOUSE, TAB),U_LT(U_SYM, RET),  U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),  U_NP,              U_NP
 
